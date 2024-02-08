bdc2f328a9cfe0456038a1afafd2919c45a5f9b1
Carissaamnd <151603130+Carissaamnd@users.noreply.github.com>
2024-02-08 22:28:42 +0700

    Add files via upload
    Belajar cara coding menggunakan bahasa pemrograman Java di NeatBeans :)

---
+++ Welcome.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.javaproject;
+
+/**
+ *
+ * @author Carissa Amanda
+ */
+public class Welcome {
+    public static void main(String[] args)
+	{
+		String greeting = "Welcome to Core Java!";
+		System.out.println(greeting);
+		for (int i = 0; i < greeting.length(); i++)
+			System.out.print("=");
+		System.out.println();
+	}
+}
